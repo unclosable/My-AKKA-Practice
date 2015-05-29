fa514924768989cc2e627f80504fbd5ffc72b8b1
zhengwei <zhengweiSE@gmail.com>
2015-05-29 15:09:59 +0800

    用新版本，读文档，跟着做！

--- explore/src/main/java/My_AKKA_Practice/explore/HelloWorld.java
+++ explore/src/main/java/My_AKKA_Practice/explore/HelloWorld.java
@@ -8,13 +8,12 @@ public class HelloWorld extends UntypedActor{
 	  public void preStart() {
 	    // create the greeter actor  
 	    final ActorRef greeter =  
-	        getContext().actorOf(Props.apply(Greeter.class), "greeter");//创建greeter actor实例  
+	        getContext().actorOf(Props.apply(Greeter.class, null), "greeter");//创建greeter actor实例  
 	    // tell it to perform the greeting  
 	    greeter.tell(Greeter.Msg.GREET, getSelf());//通过tell方法给greeter actor 发送一条消息  
 	  }  
 	@Override
 	public void onReceive(Object msg) throws Exception {
-		System.out.println(1);
 	    if (msg == Greeter.Msg.DONE) {  
 	        // when the greeter is done, stop this actor and with it the application  
 	        getContext().stop(getSelf());  
--- explore/src/main/java/My_AKKA_Practice/explore/test.java
+++ explore/src/main/java/My_AKKA_Practice/explore/test.java
@@ -1,8 +1,9 @@
 package My_AKKA_Practice.explore;
 
-import My_AKKA_Practice.explore.Pi.Listener;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Address;
+import akka.actor.Deploy;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 
@@ -11,12 +12,10 @@ public class test {
 	public static void main(String[] args) {
 		ActorSystem system=ActorSystem.create();
 
-		System.out.println(1);
-		ActorRef helloWorld=system.actorOf(new Props(HelloWorld.class), "listener");
 
-		System.out.println(2);
-		helloWorld.tell("sdf");
-		System.out.println(3);
+//		Address address=new Address("akka", "sys", "host", 1234);
+//		
+//		ActorRef ref = system.actorOf(new Props(HelloWorld.class).withDeploy(new Deploy(new RemoteScope(address))));
 		
 	}
 

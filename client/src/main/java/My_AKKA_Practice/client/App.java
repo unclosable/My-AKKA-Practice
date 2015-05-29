75408759111c103ccb491ec733b689ab29d83843
zhengwei <zhengweiSE@gmail.com>
2015-05-29 12:37:50 +0800

    试试

---
+++ client/src/main/java/My_AKKA_Practice/client/App.java
@@ -0,0 +1,13 @@
+package My_AKKA_Practice.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
